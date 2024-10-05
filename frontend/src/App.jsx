cced6348b00217fe13524bc072ceb8d7660d22fd
Francisco González <frgonzalez.415@gmail.com>
2024-10-05 17:56:04 -0300

    CREATE: Footer
    Se crea el elemento footer, se importan reglas de estilo y se instalan dependencias necesarias.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import { Footer } from './components';
 
 function App() {
 
   return (
     <>
+      <Footer />
     </>
   )
 }
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './styles/footer.scss'
+import Box from '@mui/material/Box';
+
+export const Footer = () => {
+  return (
+    <Box component='footer'>
+        FOOTER
+    </Box>
+  )
+}
