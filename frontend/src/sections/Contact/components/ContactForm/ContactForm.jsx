930ae1f3ef5c25a8200498871fb5494a518ed02b
Atilio O. Rausch <atilio.rausch@uner.edu.ar>
2024-10-10 00:16:51 -0300

    ADD: Validate form hook
    Functional form with all field validation and bot check

--- frontend/src/sections/Contact/components/ContactForm/ContactForm.jsx
+++ frontend/src/sections/Contact/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,9 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, Checkbox, FormControlLabel, TextField } from "@mui/material";
 import { ReCAPTCHAbox } from "../../../../components";
 import './styles/contactForm.scss';
 import { useState } from "react";
 import { useForm } from "../../../../hooks/useForm";
+import { useValidateForm } from "../../../../hooks/useValidateForm";
 
 export const ContactForm = () => {
 
@@ -10,15 +11,23 @@ export const ContactForm = () => {
     const [displayCAPTCHA, setDisplayCAPTCHA] = useState(false);
 
     const initialForm = {
-        name: "",
+        completename: "",
         email: "",
         subject: "",
         phone: "",
         consult: ""
     }
+    const initialFormError = {
+        errorcompletename: "",
+        erroremail: "",
+        errorsubject: "",
+        errorphone: "",
+        errorconsult: ""
+    }
 
-    const { name, email, subject, phone, consult, onInputChange } = useForm(initialForm)
-
+    const { completename, email, subject, phone, consult, onInputChange } = useForm(initialForm)
+    const { errorcompletename, erroremail, errorsubject, errorphone, errorconsult, onBlurChange } = useValidateForm(initialFormError)
+    console.log(errorcompletename);
     const handleClick = (event) => {
         event.preventDefault()
         setDisplayCAPTCHA(true);
@@ -26,21 +35,50 @@ export const ContactForm = () => {
     }
     const handleSubmit = (event) => {
         event.preventDefault();
+        if (completename === '' || email === '' || subject === '' || phone === '' || consult === '') {
+            console.log('Todos los campos deben estar completos');
+            return;
+        }
+        if (!check) {
+            console.log('Debe tildar el checkbox para continuar');
+            return;
+        }
         console.log('Se ha hecho submit en el formulario');
-        console.log(name, email, subject, phone, consult);
+        console.log(completename, email, subject, phone, consult);
     }
 
     return (
-        <Box variant='form' onSubmit={handleSubmit} className='formClass'>
-            <TextField className='textFieldClass' name='name' value={name} onChange={onInputChange} placeholder="Nombre completo" id="name" variant="standard" />
-            <TextField className='textFieldClass' name='email' value={email} onChange={onInputChange} placeholder="Email" id="email" variant="standard" />
+        <form onSubmit={ (event) => handleSubmit(event) } className='formClass'>
+            <TextField
+                error={errorcompletename}
+                color={errorcompletename ? 'error' : 'success'}
+                helperText={errorcompletename ? errorcompletename : ''}
+                className='textFieldClass' name='completename' value={completename} onBlur={onBlurChange} onChange={onInputChange} placeholder="Nombre completo" id="completename" variant="standard" />
+            <TextField
+                error={erroremail}
+                color={erroremail ? 'error' : 'success'}
+                helperText={erroremail ? erroremail : ''}
+                className='textFieldClass' name='email' value={email} onBlur={onBlurChange} onChange={onInputChange} placeholder="Email" id="email" variant="standard" />
             <Box className='containerFieldClass'>
-                <TextField fullWidth name='subject' value={subject} onChange={onInputChange} placeholder="Asunto" id="subject" variant="standard" />
-                <TextField fullWidth name='phone' value={phone} onChange={onInputChange} placeholder="Teléfono" id="phone" variant="standard" />
+                <TextField
+                    error={errorsubject}
+                    color={errorsubject ? 'error' : 'success'}
+                    helperText={errorsubject ? errorsubject : ''}
+                    fullWidth name='subject' value={subject} onBlur={onBlurChange} onChange={onInputChange} placeholder="Asunto" id="subject" variant="standard" />
+                <TextField
+                    error={errorphone}
+                    color={errorphone ? 'error' : 'success'}
+                    helperText={errorphone ? errorphone : ''}
+                    fullWidth name='phone' value={phone} onBlur={onBlurChange} onChange={onInputChange} placeholder="Teléfono" id="phone" variant="standard" />
             </Box>
-            <TextField className='textFieldClass' name='consult' value={consult} onChange={onInputChange} placeholder="Consulta" id="consult" variant="standard" multiline={true} />
-            <Button className='submitButton' type="submit" onClick={handleClick} variant="outlined">Enviar</Button>
+            <TextField
+                error={errorconsult}
+                color={errorconsult ? 'error' : 'success'}
+                helperText={errorconsult ? errorconsult : ''}
+                className='textFieldClass' name='consult' value={consult} onBlur={onBlurChange} onChange={onInputChange} placeholder="Consulta" id="consult" variant="standard" multiline={true} />
+            <FormControlLabel checked={displayCAPTCHA} control={<Checkbox />} label="No soy un botardo &#129302;" onChange={handleClick}/>
+            <Button className='submitButton' type="submit" variant="outlined">Enviar</Button>
             {displayCAPTCHA ? <ReCAPTCHAbox stateFunction={setCheck} /> : (check && false)}
-        </Box>
+        </form>
     )
 }
