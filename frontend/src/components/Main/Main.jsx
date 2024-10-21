83a434fcde0078215bd7f3ad6678a9ed3945e8c3
Atilio O. Rausch <atilio.rausch@uner.edu.ar>
2024-10-21 20:28:34 -0300

    UPDATE: Hover effects and detect section scroll

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -5,7 +5,7 @@ import './styles/main.scss'
 export const Main = () => {
   return (
     <Box component={'main'}>
-        <Inicio id="home" />
-    </Box>
+      <Inicio id="home" />
+    </Box >
   )
 }
