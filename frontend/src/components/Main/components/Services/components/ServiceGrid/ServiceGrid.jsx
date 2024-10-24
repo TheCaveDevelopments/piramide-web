abc05eb773a07fb4e3cdcb9657f0299dccc81d49
Atilio O. Rausch <atilio.rausch@uner.edu.ar>
2024-10-23 22:51:30 -0300

    REFACTORING: Services section and components Grid and Carousel

---
+++ frontend/src/components/Main/components/Services/components/ServiceGrid/ServiceGrid.jsx
@@ -0,0 +1,35 @@
+import Grid from '@mui/material/Grid2';
+import { ServiceCard } from '../';
+import './styles/ServiceGrid.scss';
+
+import PropTypes from 'prop-types';
+import { useMediaQuery } from '@mui/material';
+
+export const ServiceGrid = ({ services }) => {
+    const isSmallScreen = useMediaQuery('(max-width: 1510px)');
+    return (
+        <Grid spacing={2} container className='grid-container'>
+            {
+                services.map(service => (
+                    <Grid className='grid-item' key={service.id} size={{ xs: 12, sm: 6, md: isSmallScreen ? 6 : 3 }} >
+                        <ServiceCard
+                            key={service.id}
+                            title={service.title}
+                            description={service.description}
+                            icon={service.icon} />
+                    </Grid>
+                ))
+            }
+        </Grid>
+    );
+};
+
+
+ServiceGrid.propTypes = {
+    services: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired
+    })).isRequired
+};
--- frontend/src/components/Main/components/Services/components/index.js
+++ frontend/src/components/Main/components/Services/components/index.js
@@ -1,1 +1,3 @@
 export { ServiceCard } from './ServiceCard/ServiceCard';
+export { ServiceGrid } from './ServiceGrid/ServiceGrid';
+export { ServiceCarousel } from './ServiceCarousel/ServiceCarousel';
