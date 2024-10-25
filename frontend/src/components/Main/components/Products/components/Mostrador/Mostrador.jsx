27a1388852be9717c4ddef4ad3382898d11e1d71
Francisco González <frgonzalez.415@gmail.com>
2024-10-24 23:50:37 -0300

    UPDATE: Layout
    Se agrega dinamismo al diseño haciendo que tanto las dimensiones como las diferentes transiciones de los elementos sean armónicas entre sí para obtener un diseño limpio y fluido.

--- frontend/src/components/Main/components/Products/components/Mostrador/Mostrador.jsx
+++ frontend/src/components/Main/components/Products/components/Mostrador/Mostrador.jsx
@@ -1,27 +1,77 @@
 import Carousel from "react-material-ui-carousel";
-import { elements } from './components/elements';
+import { elements } from "./components/elements";
 import { CartaDeProducto } from "./components";
-import './styles/mostrador.scss';
-import { useEffect, useState } from "react";
+import "./styles/mostrador.scss";
+import { useEffect, useState, useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 
 export const Mostrador = () => {
+    const carouselElement = document.querySelector(".Carta");
 
-    const [carouselHeight, setCarouselHeight] = useState(null);
+  const [carouselHeight, setCarouselHeight] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const isMobile = useMediaQuery({ query: "(max-width: 819px)" });
+
+  const handleChange = (nowActive) => {
+    setActiveIndex(nowActive);
+  };
+
+  const handleResize = () => {
+    setCarouselHeight(carouselElement.scrollHeight);
+    console.log("resize");
+  };
+
+  const handleTransitionEnd = () => {
+    setCarouselHeight(carouselElement.scrollHeight);
+    console.log(
+      "transition end, updated height:",
+      carouselElement.scrollHeight
+    );
+  };
+
+  useEffect(() => {
+    if (!isMobile) {
+      window.addEventListener("resize", handleResize);
+      if (carouselElement) {
+        carouselElement.addEventListener("transitionend", handleTransitionEnd);
+      }
+    } else {
+        window.addEventListener("resize", handleResize);
+    }
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      if (carouselElement) {
+        carouselElement.removeEventListener(
+          "transitionend",
+          handleTransitionEnd
+        );
+      }
+    };
+  }, [isMobile]);
 
   return (
-    <Carousel className="Mostrador" navButtonsAlwaysVisible autoPlay={false} height={ carouselHeight }>
-        {elements.map((element, index) => (
-        <CartaDeProducto 
-            key={index} 
-            className="Carta"
-            titulo={element.titulo}
-            descripcion={element.descripcion}
-            imgPrincipal={ element.imgPrincipal }
-            imagenes={ element.img }
-            setHeight={setCarouselHeight}>
-        </CartaDeProducto>
-        ))}
+    <Carousel
+      className="Mostrador"
+      navButtonsAlwaysVisible
+      autoPlay={false}
+      height={carouselHeight}
+      onChange={handleChange}
+    >
+      {elements.map((element, index) => (
+        <CartaDeProducto
+          key={index}
+          className="Carta"
+          titulo={element.titulo}
+          descripcion={element.descripcion}
+          imgPrincipal={element.imgPrincipal}
+          imagenes={element.img}
+          index={index}
+          activeIndex={activeIndex}
+          setHeight={setCarouselHeight}
+        ></CartaDeProducto>
+      ))}
     </Carousel>
-  )
-}
+  );
+};
--- frontend/src/components/Main/components/Products/components/Mostrador/components/CartaDeProducto/CartaDeProducto.jsx
+++ frontend/src/components/Main/components/Products/components/Mostrador/components/CartaDeProducto/CartaDeProducto.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useLayoutEffect, useRef, useEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { Card, CardActionArea, CardContent, CardHeader, CardMedia, Collapse, ImageList, ImageListItem, Stack, Typography } from '@mui/material'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
@@ -9,47 +9,75 @@ export const CartaDeProducto = ({
   descripcion = "Descripcion",
   imgPrincipal = null,
   imagenes = [],
-  setHeight = () => {}
+  index = '',
+  activeIndex = 0,
+  setHeight = () => {},
 }) => {
-  
+  const nodoCartaRef = useRef(null);
+  const nodoDescripcionRef = useRef(null);
   const carta = document.querySelector('.Carta');
+
   const [image, setImage] = useState(imgPrincipal || imagenes[0]);
-  const [desplegar, setDesplegar] = useState(false);
+  const [desplegado, setDesplegado] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [restarTexto, setRestarTexto] = useState(false);
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   const isMobile = useMediaQuery({ query: '(max-width: 819px)' });
   const isTablet = useMediaQuery({ query: '(min-width: 820px) and (max-width: 1023px)' });
   const isDesktop = useMediaQuery({ query: '(min-width: 1024px) and (max-width: 1399px)' });
   const isLargeDesktop = useMediaQuery({ query: '(min-width: 1400px)' });
 
-  useEffect(() => {
-    if (carta && !isMobile) {
-      setHeight(carta.offsetHeight);
-    } else if (isMobile) {
-      setHeight(750);
+  const setCollapse = () => {
+    desplegado ? setRestarTexto(true) : setRestarTexto(false);
+    setDesplegado(!desplegado)
+  }
+
+  const handleImageLoad = () => {
+    const image = document.querySelector('.Carta img');
+
+    if (image.complete) {
+      console.log('image loaded');
+      setImageLoaded(true);
     }
-  }, [isMobile, isTablet, isDesktop, isLargeDesktop, desplegar, setHeight]);
+  };
+
+  useEffect(() => {
+    setIsActive(activeIndex === index);
+    if (!isActive && desplegado) 
+      setCollapse();
+  }, [activeIndex, index]);
+
+  useEffect(() => {
+    if (imageLoaded && isActive) {
+    if (nodoCartaRef.current && !desplegado) {
+      setHeight( restarTexto ? nodoCartaRef.current.offsetHeight - nodoDescripcionRef.current.offsetHeight : nodoCartaRef.current.offsetHeight);
+    } else if (nodoCartaRef.current && desplegado) {
+      setHeight(nodoCartaRef.current.offsetHeight + nodoDescripcionRef.current.offsetHeight);
+    }
+  }}, [isMobile, desplegado,imageLoaded]);
 
   return (
-    <Card className="Carta">
+    <Card className="Carta" id={`carta-${index}`} ref={nodoCartaRef}>
         <Stack direction={ isLargeDesktop ? 'row' : 'column'} className='Pila' justifyContent="center" alignItems="space-evenly">
 
           { !isLargeDesktop && <CardHeader title={titulo} style={{textAlign: 'center'}}/>}
           
-            <CardMedia className="Imagen" component='img' image={image}/>
+            <CardMedia className="Imagen" component='img' image={image} onLoad={handleImageLoad}/>
           
             <CardContent className="Descripcion" style={ !isLargeDesktop ? {flexDirection: 'column-reverse', width:'fit-content', minWidth: '500px'} : {}}>
           
               { isLargeDesktop && <CardHeader title={titulo} /> }
               
               {isMobile && 
-                <CardActionArea className="VerDescripcion" onClick={() => setDesplegar(!desplegar)}>
+                <CardActionArea className="VerDescripcion" onClick={() => setCollapse() } onChange={() => !desplegado ? setRestarTexto(true) : setRestarTexto(false) }>
                     <Typography variant='body1'>Ver descripción</Typography>
-                    <KeyboardArrowDownIcon style={desplegar ? {transform: 'rotate(180deg)', transition: 'transform 0.5s ease'} : {transition: 'transform 0.5s ease'}}/>
+                    <KeyboardArrowDownIcon style={desplegado ? {transform: 'rotate(180deg)', transition: 'transform 0.5s ease'} : {transition: 'transform 0.5s ease'}}/>
                 </CardActionArea>
               }
           
-              <Collapse in={!isMobile || desplegar} timeout={1500}>
-                        {<Typography variant='body1'>{descripcion}</Typography>}
+              <Collapse in={!isMobile || desplegado} timeout={1500}>
+                        {<Typography variant='body1'  ref={nodoDescripcionRef}>{descripcion}</Typography>}
               </Collapse>
               { !imgPrincipal && 
                 <ImageList className="ImageList" cols={4} gap={0}>
