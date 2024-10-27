098e08fc2c832a8c380e11b0509fc55b169d6582
Atilio O. Rausch <atilio.rausch@uner.edu.ar>
2024-10-27 18:30:27 -0300

    FIX: Main imports

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,13 +1,12 @@
-import { Home, , Contact } from "./components"
 import { Box } from "@mui/material"
-import { Inicio, Contact, Products, AboutUs, Services } from "./components"
+import { Home, Contact, Products, AboutUs, Services } from "./components"
 
 import './styles/main.scss'
 
 export const Main = () => {
   return (
     <Box component={'main'}>
-        <Inicio />
+        <Home />
         <AboutUs />
         <Services />
         <Products />
